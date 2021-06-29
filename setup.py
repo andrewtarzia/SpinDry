44ceac120b9c6f4edd8777a5712befdb67b1e9e0
Andrew Tarzia <andrew.tarzia@gmail.com>
2021-06-29 21:06:58 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="SpinDry",
-    version="0.0.1",
+    version="0.0.2",
     author="Andrew Tarzia",
     author_email="andrew.tarzia@gmail.com",
     description="Contains MC algorithm for generating host-guest conformers.",
