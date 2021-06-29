b50099301e0b8aae0f5cd31f0ac912451f169a05
Andrew Tarzia <andrew.tarzia@gmail.com>
2021-06-29 16:14:19 +0100

    Add bonds.

--- spindry/__init__.py
+++ spindry/__init__.py
@@ -2,4 +2,5 @@ from .spinner import *  # noqa
 from .molecule import *  # noqa
 from .supramolecule import *  # noqa
 from .atom import *  # noqa
+from .bond import *  # noqa
 from .utilities import *  # noqa
