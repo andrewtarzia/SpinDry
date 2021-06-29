3fc73850e0cc01c08000795f55a2e33ef35408a7
Andrew Tarzia <andrew.tarzia@gmail.com>
2021-06-29 20:19:57 +0100

    Update examples.

--- examples/multi_guest_example.py
+++ examples/multi_guest_example.py
@@ -1,22 +1,64 @@
+import stk
 import spindry as spd
 
-host = spd.Molecule('eg_host.xyz')
-guest = spd.Molecule('eg_guest.xyz')
-guest2 = spd.Molecule('eg_guest2.xyz')
-host = host.with_centroid([0, 0, 0])
-guest = guest.with_centroid([0, 0, 0])
-guest2 = guest2.with_centroid([0, 0, 0])
-print(host, guest, guest2)
+# Building a cage from the examples on the stk docs.
+bb1 = stk.BuildingBlock(
+    smiles='O=CC(C=O)C=O',
+    functional_groups=[stk.AldehydeFactory()],
+)
+bb2 = stk.BuildingBlock('NCCN', [stk.PrimaryAminoFactory()])
+
+cage = stk.ConstructedMolecule(
+    topology_graph=stk.cage.FourPlusSix(
+        building_blocks=(bb1, bb2),
+    ),
+)
+cage.write('poc.mol')
+stk_guests = (
+    stk.BuildingBlock('CN1C=NC2=C1C(=O)N(C(=O)N2C)C'),
+    stk.BuildingBlock('c1ccccc1'),
+)
+
+host_guest = stk.ConstructedMolecule(
+    topology_graph=stk.host_guest.Complex(
+        host=stk.BuildingBlock.init_from_molecule(cage),
+        guests=[stk.host_guest.Guest(i) for i in stk_guests],
+    )
+)
+host_guest.write('host_multi_guest.mol')
+
+supramolecule = spd.SupraMolecule(
+    atoms=(
+        spd.Atom(
+            id=atom.get_id(),
+            element_string=atom.__class__.__name__,
+        ) for atom in host_guest.get_atoms()
+    ),
+    bonds=(
+        spd.Bond(
+            id=i,
+            atom_ids=(
+                bond.get_atom1().get_id(),
+                bond.get_atom2().get_id(),
+            )
+        ) for i, bond in enumerate(host_guest.get_bonds())
+    ),
+    position_matrix=host_guest.get_position_matrix(),
+)
+print(supramolecule)
 
 cg = spd.Spinner(
-    step_size=0.5,
+    step_size=1,
     rotation_step_size=5,
     num_conformers=10,
 )
 
-for conformer in cg.get_conformers(host, [guest, guest2]):
-    print(conformer)
-    print(conformer.get_cid(), conformer.get_potential())
-    conformer.write_xyz_file(
-        f'multi_example_output/conf_{conformer.get_cid()}.xyz'
-    )
+conformer = cg.get_final_conformer(supramolecule)
+print(conformer)
+print(conformer.get_cid(), conformer.get_potential())
+
+# Write optimised structure out.
+opt_host_guest = host_guest.with_position_matrix(
+    conformer.get_position_matrix()
+)
+opt_host_guest.write('host_multi_guest_out.mol')
