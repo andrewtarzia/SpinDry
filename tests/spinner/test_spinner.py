e55e68d8550749f5791eca4e11c415ff7f6ce545
Andrew Tarzia <andrew.tarzia@gmail.com>
2022-02-01 13:17:42 +0000

    Add effect of sigma test.

--- tests/spinner/test_spinner.py
+++ tests/spinner/test_spinner.py
@@ -1,14 +1,23 @@
 import numpy as np
 
 
-def test_nonbond_potential(spinner, nonbond_potentials):
-    for i, d in enumerate([1, 1.2, 1.4, 1.6, 1.8, 2.0, 4]):
+def test_nonbond_potential(
+    spinner, distances, nonbond_potentials, nb_mins,
+):
+
+    for i, d in enumerate(distances):
         test = spinner._nonbond_potential(
             distance=d,
             sigmas=np.array(1.2),
         )
         assert np.isclose(test, nonbond_potentials[i], atol=1E-5)
 
+    for i, _sigma in enumerate([1.2, 1.4, 1.8, 1.6]):
+        test = spinner._nonbond_potential(
+            distance=distances,
+            sigmas=np.array(_sigma),
+        )
+        assert np.argmin(test**2) == nb_mins[i]
 
 def test_opt(spinner, smolecule, final_pos_mat, final_potential):
     test = spinner.get_final_conformer(smolecule)
