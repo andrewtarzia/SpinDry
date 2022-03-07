3f055eed98fd124d3ed6fa5a1d35e2e1d8f961fa
Andrew Tarzia <andrew.tarzia@gmail.com>
2022-03-07 19:59:46 +0000

    Add an example.

---
+++ examples/component_example.py
@@ -0,0 +1,93 @@
+import stk
+import spindry as spd
+
+# Building a cage from the examples on the stk docs.
+bb1 = stk.BuildingBlock(
+    smiles='O=CC(C=O)C=O',
+    functional_groups=[stk.AldehydeFactory()],
+)
+bb2 = stk.BuildingBlock('NCCN', [stk.PrimaryAminoFactory()])
+
+cage = stk.ConstructedMolecule(
+    topology_graph=stk.cage.FourPlusSix(
+        building_blocks=(bb1, bb2),
+    ),
+)
+stk_guests = (stk.BuildingBlock('C1CCCCC1'), )
+
+host_guest = stk.ConstructedMolecule(
+    topology_graph=stk.host_guest.Complex(
+        host=stk.BuildingBlock.init_from_molecule(cage),
+        guests=[stk.host_guest.Guest(i) for i in stk_guests],
+    )
+)
+
+host_molecule = spd.Molecule(
+    atoms=(
+        spd.Atom(
+            id=atom.get_id(),
+            element_string=atom.__class__.__name__,
+        ) for atom in cage.get_atoms()
+    ),
+    bonds=(
+        spd.Bond(
+            id=i,
+            atom_ids=(
+                bond.get_atom1().get_id(),
+                bond.get_atom2().get_id(),
+            )
+        ) for i, bond in enumerate(
+            cage.get_bonds()
+        )
+    ),
+    position_matrix=(
+        cage.get_position_matrix()
+    ),
+)
+guest_molecule = spd.Molecule(
+    atoms=(
+        spd.Atom(
+            id=atom.get_id(),
+            element_string=atom.__class__.__name__,
+        ) for atom in stk_guests[0].get_atoms()
+    ),
+    bonds=(
+        spd.Bond(
+            id=i,
+            atom_ids=(
+                bond.get_atom1().get_id(),
+                bond.get_atom2().get_id(),
+            )
+        ) for i, bond in enumerate(stk_guests[0].get_bonds())
+    ),
+    position_matrix=stk_guests[0].get_position_matrix(),
+)
+
+supramolecule = spd.SupraMolecule.init_from_components(
+    components=(host_molecule, guest_molecule),
+)
+cg = spd.Spinner(
+    step_size=0.2,
+    rotation_step_size=1.0,
+    num_conformers=5,
+)
+
+for conformer in cg.get_conformers(supramolecule):
+    pass
+
+# Write optimised structures out.
+for i, comp in enumerate(list(conformer.get_components())):
+    if i == 0:
+        new_cage = cage.with_position_matrix(
+            comp.get_position_matrix()
+        )
+        new_cage.write(
+            f'comp_new_host.mol'
+        )
+    elif i == 1:
+        new_guest = stk_guests[0].with_position_matrix(
+            comp.get_position_matrix()
+        )
+        new_guest.write(
+            f'comp_new_guest.mol'
+        )
