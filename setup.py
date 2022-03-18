b0e950765fa4124a11be8f0a4384975f0b1f7d70
Andrew Tarzia <andrew.tarzia@gmail.com>
2022-03-18 17:18:45 +0000

    Bump version. Add version method.

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
-import setuptools
+from setuptools import find_packages, setup
+import re
+from os.path import join
 
-setuptools.setup(
+
+def get_version():
+    with open(join('src', 'stk', '__init__.py'), 'r') as f:
+        content = f.read()
+    p = re.compile(r'^__version__ = [\'"]([^\'\"]*)[\'"]', re.M)
+    return p.search(content).group(1)
+
+
+setup(
     name="SpinDry",
-    version="0.0.9",
+    version="0.0.91",
     author="Andrew Tarzia",
     author_email="andrew.tarzia@gmail.com",
     description="Contains MC algorithm for generating host-guest conformers.",
     url="https://github.com/andrewtarzia/SpinDry",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     install_requires=(
         'scipy',
         'matplotlib',
